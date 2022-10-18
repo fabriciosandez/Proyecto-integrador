fb68bd48fa08691b1d5f8b9744975c30697fd45f
fabriciosandez <fsandez23@gmail.com>
2022-10-18 13:16:25 -0300

    Cambios 13:16

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
